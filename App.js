583ddbf5bf049a1ed990f87c59c0b7cbc8a08e4f
bockepechen <bockepechen@163.com>
2018-02-01 18:33:47 +0800

    add navigator

--- App.js
+++ App.js
@@ -9,30 +9,68 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import TabNavigator from 'react-native-tab-navigator';
+let popularIcon = require('./res/images/ic_polular.png');
+let trendingIcon = require('./res/images/ic_trending.png');
+let favoriteIcon = require('./res/images/ic_favorite.png');
+let myIcon = require('./res/images/ic_my.png');
+const staticProp = {
+  popular: 'Popular',
+  trending: 'Trending',
+  favorite: 'Favorite',
+  my: 'My'
+};
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: staticProp.popular
+    };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!(Window -> Mac update)
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <TabNavigator>
+        <TabNavigator.Item 
+          selected={this.state.selectedTab === staticProp.popular}
+          selectedTitleStyle={{color:'#436EEE'}}
+          title={staticProp.popular}
+          renderIcon={() => <Image style={styles.tabIcon} source={popularIcon} />}
+          renderSelectedIcon={() => <Image style={[styles.tabIcon,{tintColor:'#436EEE'}]} source={popularIcon} />}
+          onPress={() => this.setState({selectedTab:staticProp.popular})} >
+          <View style={{flex:1, justifyContent:'center'}}><Text>Popular</Text></View>
+        </TabNavigator.Item>
+        <TabNavigator.Item 
+          selected={this.state.selectedTab === staticProp.trending}
+          selectedTitleStyle={{color:'#436EEE'}}
+          title={staticProp.trending}
+          renderIcon={() => <Image style={styles.tabIcon} source={trendingIcon} />}
+          renderSelectedIcon={() => <Image style={[styles.tabIcon,{tintColor:'#436EEE'}]} source={trendingIcon} />}
+          onPress={() => this.setState({selectedTab:staticProp.trending})} >
+          <View style={{flex:1, justifyContent:'center'}}><Text>Trending</Text></View>
+        </TabNavigator.Item>
+        <TabNavigator.Item 
+          selected={this.state.selectedTab === staticProp.favorite}
+          selectedTitleStyle={{color:'#436EEE'}}
+          title={staticProp.favorite}
+          renderIcon={() => <Image style={styles.tabIcon} source={favoriteIcon} />}
+          renderSelectedIcon={() => <Image style={[styles.tabIcon,{tintColor:'#436EEE'}]} source={favoriteIcon} />}
+          onPress={() => this.setState({selectedTab:staticProp.favorite})} >
+          <View style={{flex:1, justifyContent:'center'}}><Text>Favorite</Text></View>
+        </TabNavigator.Item>
+        <TabNavigator.Item 
+          selected={this.state.selectedTab === staticProp.my}
+          selectedTitleStyle={{color:'#436EEE'}}
+          title={staticProp.my}
+          renderIcon={() => <Image style={styles.tabIcon} source={myIcon} />}
+          renderSelectedIcon={() => <Image style={[styles.tabIcon,{tintColor:'#436EEE'}]} source={myIcon} />}
+          onPress={() => this.setState({selectedTab:staticProp.my})} >
+          <View style={{flex:1, justifyContent:'center'}}><Text>My</Text></View>
+        </TabNavigator.Item>
+      </TabNavigator>
     );
   }
 }
@@ -40,18 +78,14 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  page: {
+    flex: 1,
+    backgroundColor: 'red',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  tabIcon: {
+    width: 22,
+    height: 22
+  }
 });
